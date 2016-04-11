23db4792cfd3519201e5637aede22edc3c261aa0
WANG Siyuan <wangsiyuanbuaa@gmail.com>
2016-04-12 04:16:02 +0800

    username, code style

--- smallyellowbot.lua
+++ smallyellowbot.lua
@@ -20,42 +20,75 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 ]]
 
+--[[
+Code Style:
+  1. lowerCamelCase
+  2. Indent 2 spaces
+--]]
 -- pass token as command line argument or insert it into code
 local token = arg[1] or "134452794:AAFmTkfgaLU58OJh7hxL3tMytcBB5A0cKpo"
 
 -- create and configure new bot with set token
 local bot, extension = require("lua-bot-api").configure(token)
 
-lastcmd = {}
+local lastCmd = {}
+local forceReply = bot.generateForceReply(true, true)
+
+function letterNumberUnderscore(str)
+  words = {}
+  for w in string.gmatch(str, "[%w_]+") do table.insert(words, w) end
+  return words[1]
+end
 -- override onMessageReceive function so it does what we want
 extension.onMessageReceive = function (msg)
-	print("New Message by " .. msg.from.first_name)
-	cmd = {}
-	if (lastcmd[msg.from.id]) then
-		cmd[1] = lastcmd[msg.from.id]
-		cmd[2] = msg.text
-		--table.remove(lastcmd, msg.from.id)
-		lastcmd[msg.from.id] = nil
-	else
-		for word in msg.text:gmatch("%S+") do table.insert(cmd, word) end
-	end
+  print("New Message by " .. msg.from.first_name)
+  if (msg.from.username) then
+    print("netx username " .. msg.from.username .. "xx")
+  end
+  cmd = {}
 
-	replayname = (msg.from.username and ("@" .. msg.from.username .. " ") or msg.from.first_name)
+  if(msg.from.username and lastCmd[msg.from.username]) then
+    print("mmmmmmmmmmmmm " .. lastCmd[msg.from.username])
+    lastCmd["@" .. msg.from.username] = nil
+    return
+  elseif (lastCmd[msg.from.id]) then
+    cmd[1] = lastCmd[msg.from.id]
+    cmd[2] = msg.text
+    lastCmd[msg.from.id] = nil
+  else
+    for word in msg.text:gmatch("%S+") do table.insert(cmd, word) end
+  end
 
-	if (cmd[1] == "/print") then
-		if (cmd[2]) then
-			bot.sendMessage(msg.chat.id,
-			replayname .. "说" .. cmd[2], nil, nil, nil, nil, nil)
-		else
-			lastcmd[msg.from.id] = "/print"
-			forcereply = bot.generateForceReply(true, true)
-			bot.sendMessage(msg.chat.id, 
-			replayname .. "你想说啥", nil, nil, nil, 
-			msg.message_id, forcereply)
-		end
-	else
-		bot.sendMessage(msg.chat.id, "不知道" .. replayname ..  "在说什么")
-	end
+  replayName = (msg.from.username and ("@" .. msg.from.username .. " ") or msg.from.first_name)
+
+  if (cmd[1] == "/print") then
+    if (cmd[2]) then
+      bot.sendMessage(msg.chat.id,
+      replayName .. "说" .. cmd[2], nil, nil, nil, nil, nil)
+    else
+      lastCmd[msg.from.id] = "/print"
+      bot.sendMessage(msg.chat.id, 
+      replayName .. "你想说啥", nil, nil, nil, 
+      msg.message_id, forceReply)
+    end
+  elseif (cmd[1] == "/memeda") then
+    if (cmd[2]) then
+      replykeyboard = bot.generateReplyKeyboardMarkup({{replayName .. "你快来，人家想要！"},{replayName .. "你走开！"}},
+      true, true, true)
+      print("11111 cmd[2]:" .. cmd[2] .. "xx")
+      print("22222 cmd[2]:" .. letterNumberUnderscore(cmd[2]) .. "xx")
+      lastCmd[letterNumberUnderscore(cmd[2])] = "replay@" .. cmd[1]
+      bot.sendMessage(msg.chat.id,
+      cmd[2] .. " " .. replayName .. "要么么哒你！", nil, nil, nil, nil, replykeyboard)
+    else
+      lastCmd[msg.from.id] = cmd[1]
+      bot.sendMessage(msg.chat.id, 
+      replayName .. "你想么么哒谁", nil, nil, nil, 
+      msg.message_id, forceReply)
+    end
+  else
+    bot.sendMessage(msg.chat.id, "不知道" .. replayName ..  "在说什么")
+  end
 end
 
 -- This runs the internal update and callback handler
